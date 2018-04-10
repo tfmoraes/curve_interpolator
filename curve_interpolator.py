1c7ebebd29d45653ffad993a17b3c29256f17085
Thiago Franco de Moraes <totonixsame@gmail.com>
2018-04-10 16:21:32 -0300

    first commit

---
+++ curve_interpolator.py
@@ -0,0 +1,119 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy import interpolate
+from scipy.misc import imread
+
+import ply_writer
+
+def img2points(img):
+    bw_img = (img > 127) * 1
+    points = []
+    for y in xrange(bw_img.shape[0]):
+        for x in xrange(bw_img.shape[1]):
+            if bw_img[y, x]:
+                points.append((x, y))
+                break
+        if points:
+            break
+
+    xi, yi = points[-1]
+    while True:
+        xn, yn = points[-1]
+        nexts = [(xn+i, yn+j) for i,j in ((+1, 0), (0, +1), (-1, 0), (0, -1),
+                                          (+1, +1), (-1, +1), (-1, -1), (+1, -1)) \
+                 if ((0 < xn + i < img.shape[1]) and (0 < yn + j < img.shape[0]))]
+        appended = False
+        for x, y in nexts:
+            if bw_img[y, x] == 1:
+                bw_img[y, x] = 2
+                points.append((x, y))
+                appended = True
+                break
+
+        if not appended:
+            return points
+
+
+def normalize_curve(points, npoints):
+    px = points[:, 0]
+    py = points[:, 1]
+
+    t = np.linspace(0, 1, len(points))
+    fx = interpolate.CubicSpline(t, px)
+    fy = interpolate.CubicSpline(t, py)
+
+    nt = np.linspace(0, 1, npoints)
+    npx = fx(nt)
+    npy = fy(nt)
+
+    return npx, npy
+
+
+def interpolate_curve(px1, py1, px2, py2, z):
+    px3 = np.zeros(shape=(z.shape[0], px1.shape[0]))
+    py3 = np.zeros(shape=(z.shape[0], py1.shape[0]))
+
+    t = np.array((0.0, 1.0))
+
+    for i in xrange(px1.shape[0]):
+        fx = interpolate.CubicSpline(t, np.array((px1[i], px2[i])))
+        fy = interpolate.CubicSpline(t, np.array((py1[i], py2[i])))
+
+        px3[:, i] = fx(z)
+        py3[:, i] = fy(z)
+
+    return px3, py3
+
+
+def to3dsurface(px, py, pz):
+    points = np.zeros(shape=(px.size, 3))
+    faces = []
+    k = 0
+    for i in xrange(px.shape[0]):
+        for j in xrange(px.shape[1]):
+            x = px[i, j]
+            y = py[i, j]
+            z = pz[i]
+            points[k] = x, y, z
+            k += 1
+
+    for i in xrange(px.shape[0]-1):
+        for j in xrange(px.shape[1]):
+            faces.append((i*px.shape[1] + j, i*px.shape[1] + j + 1, (i+1)*px.shape[1] + j))
+            faces.append((i*px.shape[1] + j + 1, (i+1)*px.shape[1] + j + 1, (i+1)*px.shape[1] + j))
+
+    return points, np.array(faces)
+
+def main():
+    img1 = imread(sys.argv[1])[:, :, 0]
+    points1 = np.array(img2points(img1), dtype='float64')
+    points1[:, 0] = points1[:, 0] / float(img1.shape[1])
+    points1[:, 1] = points1[:, 1] / float(img1.shape[0])
+
+    print points1
+
+    img2 = imread(sys.argv[2])[:, :, 0]
+    points2 = np.array(img2points(img2), dtype='float64')
+    points2[:, 0] = points2[:, 0] / float(img2.shape[1])
+    points2[:, 1] = points2[:, 1] / float(img2.shape[0])
+
+    npx1, npy1 = normalize_curve(points1, 100)
+    npx2, npy2 = normalize_curve(points2, 100)
+
+    npz3 = np.linspace(0, 1, 100)
+    npx3, npy3 = interpolate_curve(npx1, npy1, npx2, npy2, npz3)
+
+    points, faces = to3dsurface(npx3, npy3, npz3)
+
+    writer = ply_writer.PlyWriter(sys.argv[3])
+    writer.from_faces_vertices_list(faces, points)
+    plt.plot(npx1, npy1, label='curve 1')
+    plt.plot(npx2, npy2, label='curve 2')
+    for i in xrange(10):
+        plt.plot(npx3[i], npy3[i], label='curve %d' % i)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
---
+++ ply_writer.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import math
+
+class PlyWriter(object):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def _write_header(self, ply_file, n_vertices, n_faces, has_normal_vertex=False, has_colours=False):
+        ply_file.write('ply\n')
+        ply_file.write('format ascii 1.0\n')
+        ply_file.write('element vertex %d\n' % n_vertices)
+        ply_file.write('property float x\n')
+        ply_file.write('property float y\n')
+        ply_file.write('property float z\n')
+
+        if has_normal_vertex:
+            ply_file.write('property float nx\n')
+            ply_file.write('property float ny\n')
+            ply_file.write('property float nz\n')
+
+        if has_colours:
+            ply_file.write('property uchar red\n')
+            ply_file.write('property uchar green\n')
+            ply_file.write('property uchar blue\n')
+            ply_file.write('property uchar alpha\n')
+
+        ply_file.write('element face %d\n' % n_faces)
+        ply_file.write('property list uchar int vertex_indices\n')
+        ply_file.write('end_header\n')
+
+    def from_faces_vertices_list(self, faces, vertices, vnormals=None, colours=None):
+        with file(self.filename, 'w') as ply_file:
+            self._write_header(ply_file, len(vertices), len(faces), vnormals is not None, colours is not None)
+            for k, v in enumerate(vertices):
+                if vnormals is not None:
+                    ply_file.write((' '.join(['%f' % i for i in v[:3]])))
+                    nz, ny, nx = vnormals[k]
+                    s = math.sqrt(nz*nz + ny*ny + nx*nx)
+                    ply_file.write(' %f %f %f\n' % (nx/s, ny/s, nz/s))
+                elif colours is not None:
+                    ply_file.write((' '.join(['%f' % i for i in v[:3]])))
+                    try:
+                        ply_file.write(' %d %d %d 255\n' % colours[k])
+                    except KeyError:
+                        ply_file.write(' %d %d %d 255\n' % (0, 0, 0))
+                else:
+                    ply_file.write((' '.join(['%f' % i for i in v[:3]]) + '\n'))
+
+            for face in faces:
+                ply_file.write('%d %s\n' % (len(face), ' '.join(['%d' % i for i in face])))
